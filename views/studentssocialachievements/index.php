a58d1a33e1ff035cbea8d6f6a83e05ded72d89b7
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2019-10-14 03:12:19 +0700

    ingachievements

--- views/studentssocialachievements/index.php
+++ views/studentssocialachievements/index.php
@@ -6,7 +6,7 @@ use yii\grid\GridView;
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Учебный план';
+$this->title = 'Общественные достижения студента';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="studentssocialachievements-index">
