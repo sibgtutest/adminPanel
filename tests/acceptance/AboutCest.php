b08b132837ba8cb60c7c4980509d46f03756a956
inom124 <inomzhon.abdulloev@gmail.com>
2019-12-10 15:06:51 +0700

    tests

--- tests/acceptance/AboutCest.php
+++ tests/acceptance/AboutCest.php
@@ -6,7 +6,37 @@ class AboutCest
 {
     public function ensureThatAboutWorks(AcceptanceTester $I)
     {
-        $I->amOnPage(Url::toRoute('/site/about'));
-        $I->see('About', 'h1');
+        $I->amOnPage(Url::toRoute('/lfsibgu/index'));
+        $I->see('Основные сведения', 'h1');
+
+         $I->amOnPage(Url::toRoute('/lfsibgu/news'));
+         $I->see('Конкурс на замещение вакантных должностей!', 'a'); 
+
+         $I->amOnPage(Url::toRoute('/lfsibgu/elements'));
+        $I->see('Торжественная линейка к Дню знаний!', 'a');
+
+        $I->amOnPage(Url::toRoute('/lfsibgu/elementarno'));
+        $I->see('Продолжить обучение в магистратуре ');
+        
+        $I->amOnPage(Url::toRoute('/lfsibgu/element'));
+         $I->see('Студенту', 'a');
+        
+        $I->amOnPage(Url::toRoute('/lfsibgu/elementchik'));
+        $I->see(' Документы первичной профсоюзной организации', 'a');
+
+        $I->amOnPage(Url::toRoute('/lfsibgu/elementy'));
+        $I->see('Электронная информационно-образовательная среда ', 'strong');
+
+        $I->amOnPage(Url::toRoute('/lfsibgu/elementov'));
+        $I->see('Фотогалерея', 'a');
+
+        $I->amOnPage(Url::toRoute('/lfsibgu/elemento'));
+        $I->see('Ноутбуки', 'h1');
+        
+        $I->amOnPage(Url::toRoute('/lfsibgu/elementu'));
+        $I->see('Блейд - разбор анонса Marvel. Чем удивит пятая фаза киновселенной?', 'h1');
+
+        $I->amOnPage(Url::toRoute('/lfsibgu/elementm'));
+        $I->see('faridlesosibirsk', 'h1');    
     }
 }
